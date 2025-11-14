7b51d965fad59d88bfc33a2288004a4d2df408df
Mawojast <123590044+Mawojast@users.noreply.github.com>
2025-11-14 20:36:49 +0100

    fixed route links

--- resources/js/pages/pol/TermsOfService.vue
+++ resources/js/pages/pol/TermsOfService.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
-import LightDarkSwitch from '@/components/LightDarkSwitch.vue';
+import dashboard from '@/routes/dashboard';
+import { register, login } from '@/routes'
 const data = defineProps(['htmlText']);
 </script>
 
@@ -18,20 +19,20 @@ const data = defineProps(['htmlText']);
                 <nav class="flex  justify-end gap-6">
                     <Link
                     v-if="$page.props.auth.user"
-                    :href="route('dashboard.index')"
+                    :href="dashboard.index()"
                     class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] dark:text-[#EDEDEC] hover:border-[#1915014a] dark:border-[#3E3E3A]  dark:hover:border-[#62605b]"
                     >
                         Dashboard
                     </Link>
                     <template v-else>
                         <Link
-                        :href="route('login')"
+                        :href="login()"
                         class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                         >
                             Log in
                         </Link>
                         <Link
-                        :href="route('register')"
+                        :href="register()"
                         class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                         >
                             Register
